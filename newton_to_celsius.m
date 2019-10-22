feef5d72aa2fe33e1bd9e6bc399b0ef21ffa7b5e
zhxutong <xutong.zhang@manchester.ac.uk>
2019-10-22 22:10:18 +0100

    Add Newton to Celsius conversion

---
+++ newton_to_celsius.m
@@ -0,0 +1,5 @@
+function celsius = newton_to_celsius(newton)
+    %NEWTON_TO_CELSIUS   Convert Newton scale to Celsius
+
+    celsius = 100/33 * newton;
+end
--- temperature_conversions.m
+++ temperature_conversions.m
@@ -7,4 +7,5 @@ disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(1
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
 fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg) 
-fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg)
+
+fprintf('33 %sN is %g%sC.\n', deg, newton_to_celsius(33), deg)
