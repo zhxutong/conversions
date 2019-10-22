cfbec5573c06a94e5446732ff3a04b27dda590de
zhxutong <xutong.zhang@manchester.ac.uk>
2019-10-22 21:10:56 +0100

    Add script to print key conversions.

---
+++ temperature_conversions.m
@@ -0,0 +1,7 @@
+%TEMPERATURE_CONVERSIONS
+
+% Save degree symbol as a variable
+deg = char(176);
+
+disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
+fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
