2ae2382940b9c25f494633d0291a31095b9d71d0
zhxutong <xutong.zhang@manchester.ac.uk>
2019-10-22 21:14:19 +0100

    Include Kelvin conversion

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -5,3 +5,4 @@ deg = char(176);
 
 disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
+fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
