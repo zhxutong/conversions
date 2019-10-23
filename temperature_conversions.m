bb24c726aad09dc9d69ce0892c5fa346edc59e45
zhxutong <xutong.zhang@manchester.ac.uk>
2019-10-23 08:48:05 +0100

    Add plot section

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -10,3 +10,9 @@ fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg) 
 
 fprintf('33 %sN is %g%sC.\n', deg, newton_to_celsius(33), deg)
+
+% Plot Fahrenheit vs Celsius
+degC = linspace(0,100,101);
+plot(degC, celsius_to_fahrenheit(degC))
+xlabel('Celsius')
+ylabel('Fahrenheit')
